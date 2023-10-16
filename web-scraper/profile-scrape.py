d380bd8c4e0eb7a7cf11b2f85fc71ce346e212b0
sabrinaaquino <sabrina.aquino@hotmail.com>
2023-10-16 16:16:08 -0300

    fixed the scraper

--- web-scraper/profile-scrape.py
+++ web-scraper/profile-scrape.py
@@ -1,86 +1,44 @@
-# Scrape Github profile data
-### Creates CSV file with commit data from profile data visualization
-### Creates JSON file with basic profile information and highlights
-### Downloads avatar photo
-
-import urllib
+import requests
 from bs4 import BeautifulSoup
-import sys
-import csv
-import json
-import os
-import shutil
+import datetime
 
-output = []
-gitUser = sys.argv[1]
-url = 'http://github.com/' + gitUser
-data = urllib.urlopen(url).read()
-soup = BeautifulSoup(data)
+today = datetime.datetime.today().strftime('%Y-%m-%d')
 
-### make a new folder for each user
-os.getcwd()
-os.mkdir(gitUser)
-os.chdir(gitUser)
+def check_user_contribution(username, date):
+  """Checks if a user has contributed to any GitHub repository on a specific day.
 
+  Args:
+    username: The username of the user to check.
+    date: The date to check (in YYYY-MM-DD format).
 
+  Returns:
+    True if the user has contributed to at least one repository on the specified date, False otherwise.
+  """
 
-### get commit history data, write to CSV
-f = open(gitUser + '.csv', 'wt')
+  url = f'https://github.com/{username}/contributions?from={date}&to={date}'
+  response = requests.get(url)
+  soup = BeautifulSoup(response.content, 'html.parser')
 
-days = soup.findAll('rect', {'class' : 'day'})
-writer = csv.writer(f)
-writer.writerow(('Commit Count', 'Date'))
+  # Check if the user has any contributions on the specified date.
+  contributions = soup.find_all('rect', class_='contribution calendar-day')
+  for contribution in contributions:
+    if contribution['data-date'] == date:
+      return True
 
+  return False
 
-for day in days:
-	count = day['data-count']
-	date = day['data-date']
-	writer.writerow((count,date))
+def main():
+  """Checks if the user Bard has contributed to any repository on today's date."""
 
+  username = 'sabrinaaquino'
+  date = today
 
-f.close()
+  has_contributed = check_user_contribution(username, date)
 
+  if has_contributed:
+    print('The user has contributed to at least one repository on today\'s date.')
+  else:
+    print('The user has not contributed to any repository on today\'s date.')
 
-
-
-### get avatar
-def download_image(imageurl):
-	print ('downloading ', imageurl)
-	#formatting the url to get full-sized images
-	DLurl = imageurl.replace("?v=3&s=460", "")
-	filename = gitUser + ".jpeg"
-	urllib.urlretrieve(DLurl,filename)
-
-
-### get profile info
-fullname = soup.find('span', {'class' : 'vcard-fullname'}).string
-username = soup.find('span', {'class' : 'vcard-username'}).string
-location = soup.find('li', {'class' : 'vcard-detail'})['title']
-joined = soup.find('time', {'class' : 'join-date'}).string
-followers = soup.find('strong', {'class' : 'vcard-stat-count'}).string
-streak = soup.findAll('span', {'class' : 'contrib-number'})[1].text
-
-
-### get repo info
-repos = soup.findAll('div', {'class' : 'columns popular-repos'})
-for reps in repos:
-	popular = reps.findAll('div', {'class' : 'column one-half'})[0]
-	contributed = reps.findAll('div', {'class' : 'column one-half'})[1]
-	
-	pops = popular.find('a', {'class' : 'mini-repo-list-item css-truncate'})['href']
-	popstars = popular.find('span', {'class' : 'stars'}).text.strip()
-
-	cons = contributed.find('a', {'class' : 'mini-repo-list-item css-truncate'})['href']
-	constars = contributed.find('span', {'class' : 'stars'}).text.strip()
-
-
-### write profile info to JSON
-output.append({'full name': fullname, 'user name': username, 'location': location, 'joined': joined, 'followers': followers, 'popular repo': pops, 'popular stars': popstars, 'contributed to': cons, 'contributed stars': constars, 'longest streak': streak})
-
-with open(gitUser + ".json", "w") as outputfile:
-	json.dump(output, outputfile, indent=2)
-	print ('dumping ', outputfile)
-
-### 
-imgLoc = soup.find("img")['src']
-download_image(imgLoc)
+if __name__ == '__main__':
+  main()
